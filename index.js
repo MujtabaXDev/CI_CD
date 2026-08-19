bbe18ec1f602dcab4ef622d89445f21f9dde8604
Muhammad Mujtaba <mujtabafarooq.dev@gmail.com>
2026-08-20 04:36:56 +0500

    CD testing updating HTML

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+// adding comment
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
